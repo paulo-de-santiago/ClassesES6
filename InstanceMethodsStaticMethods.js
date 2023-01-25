ad5d177c9b6708e147515bb793c2c3900c1ddd6b
Paulo Santiago <pauloecsantiago@gmail.com>
2023-01-25 16:10:07 +0000

    Instance methods and static methods.

---
+++ InstanceMethodsStaticMethods.js
@@ -0,0 +1,31 @@
+// @ts-nocheck
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  //Instance Method - specific to a object
+  draw() {}
+
+  // Static Method - are not available on the object instance, only accessible on the class
+  static parse(str) {
+    const radius = JSON.parse(str).radius;
+    return new Circle(radius);
+  }
+}
+
+//Cricle object instance has the draw method
+const circle = new Circle(1);
+console.log(circle);
+
+//STATIC METHODS
+const circle = Circle.parse('{"radius: 1"}');
+console.log(circle);
+//These are available on the class itself but not on the object instance. Used to create utility fucntions that are not specific to a given object
+
+class Math2 {
+  static abs(value) {
+    //
+  }
+}
+
+Math2.abs();
