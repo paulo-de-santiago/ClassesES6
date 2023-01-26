b940c779ba209307e7dd4c2d7a4b5504c578f51f
Paulo Santiago <pauloecsantiago@gmail.com>
2023-01-26 11:51:57 +0000

    Exercise stack - plan

---
+++ stackDataStructureES6.js
@@ -0,0 +1,21 @@
+// Stack, data structure, has essecially two operations: push and pop
+
+//Implement stack class
+// call new object: const stack = new Stack()
+//stack , count property, three methods in the prototype: peek(), pop(), push()
+
+//peek() returns the object at the top of the stack
+
+//push("a") count = 1
+//pop ("a") count = 0
+// stack.pop() and stack.peek()  throw new Error ("Stack is empty.")
+
+//uma variável stackPile  const _stackPile = new WeakMap() . set
+
+// _count = new WeakMap()
+// _stackPile = new WeakMap()
+// _pop = new WeakMap()
+// _push = new WeakMap()
+// peek = new WeakMap()
+
+//Define which ones go in the constructor and which ones go in the prototype
