9dfc7d7c7204abac336918b81cfbb5463ea1869f
Paulo Santiago <pauloecsantiago@gmail.com>
2023-01-30 19:30:51 +0000

    Finders keepers

---
+++ findersKeepers.js
@@ -0,0 +1,30 @@
+function findElement(arr, func) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+  return undefined;
+}
+
+console.log(
+  findElement([1, 3, 5, 8, 9, 10], function (num) {
+    return num % 2 === 0;
+  })
+); //should return 8.
+console.log(
+  findElement([1, 3, 5, 9], function (num) {
+    return num % 2 === 0;
+  })
+); //should return undefined.
+
+/* Second solution with find()
+
+
+function findElement(arr, func) {
+  return arr.find(func);
+}
+
+*/
